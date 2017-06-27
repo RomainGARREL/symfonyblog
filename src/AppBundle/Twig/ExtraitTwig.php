1762e5b61cdf00db0b654d588ae2592c005cd8fe
julien <julien3b@gmail.com>
2017-06-27 09:47:17 +0200

    Mardi 27/06/Matin 2 : On genere une fonction et son service

---
+++ src/AppBundle/Twig/ExtraitTwig.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Twig;
+
+/**
+ * Description of Extrait
+ *
+ * @author Human Booster
+ */
+class ExtraitTwig extends \Twig_Extension {
+
+    private $max;
+    private $suite;
+
+    public function __construct($max, $suite) {
+        $this->max = $max;
+        $this->suite = $suite;
+    }
+
+    public function get($texte) {
+        $texte = strip_tags($texte);
+
+        if (strlen($texte) > $this->max) {
+            $texte = substr($texte, 0, $this->max);
+            $texte = substr($texte, 0, strrpos($texte, " ")) . $this->suite;
+        }
+        return $texte;
+    }
+
+    public function getFunctions() {
+        return array(new \Twig_SimpleFunction('extrait', array($this, 'get')));
+    }
+
+}
